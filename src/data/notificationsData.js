0552c5d90976db8ee432858246da98f533ebde90
Gaurish Saini <gaurishprakhar@gmail.com>
2024-09-21 06:44:32 +0530

    feat: setup data for components

---
+++ src/data/notificationsData.js
@@ -0,0 +1,8 @@
+const notificationsData = [
+  { message: "You have a bug that needs resolving.", time: "Just now" },
+  { message: "New user registered.", time: "59 minutes ago" },
+  { message: "You have a bug that needs resolving.", time: "12 hours ago" },
+  { message: "Andi Lane subscribed to you.", time: "Today, 11:59 AM" },
+];
+
+export default notificationsData;
---
+++ src/data/totalSalesData.js
@@ -0,0 +1,8 @@
+const totalSalesData = [
+  { name: "Direct", value: 300.56 },
+  { name: "Affiliate", value: 135.18 },
+  { name: "Sponsored", value: 154.02 },
+  { name: "Email", value: 48.96 },
+];
+
+export default totalSalesData;

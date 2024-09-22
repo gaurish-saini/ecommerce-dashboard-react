deb72df1342a37c14286da0051d586431cfe760a
Gaurish Saini <gaurishprakhar@gmail.com>
2024-09-23 04:31:07 +0530

    fix: missing dependency

--- src/hooks/useKeyboardNavigation.js
+++ src/hooks/useKeyboardNavigation.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export const useKeyboardNavigation = (
   listLength,
@@ -7,30 +7,34 @@ export const useKeyboardNavigation = (
 ) => {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
-  const handleKeyDown = (e) => {
-    if (isDropdownVisible && listLength > 0) {
-      if (e.key === "ArrowDown") {
-        e.preventDefault();
-        setSelectedIndex((prevIndex) =>
-          prevIndex === listLength - 1 ? 0 : prevIndex + 1
-        );
-      } else if (e.key === "ArrowUp") {
-        e.preventDefault();
-        setSelectedIndex((prevIndex) =>
-          prevIndex <= 0 ? listLength - 1 : prevIndex - 1
-        );
-      } else if (e.key === "Enter" && selectedIndex !== -1) {
-        onEnter(selectedIndex);
+  // Memoized handleKeyDown function to avoid unnecessary re-renders
+  const handleKeyDown = useCallback(
+    (e) => {
+      if (isDropdownVisible && listLength > 0) {
+        if (e.key === "ArrowDown") {
+          e.preventDefault();
+          setSelectedIndex((prevIndex) =>
+            prevIndex === listLength - 1 ? 0 : prevIndex + 1
+          );
+        } else if (e.key === "ArrowUp") {
+          e.preventDefault();
+          setSelectedIndex((prevIndex) =>
+            prevIndex <= 0 ? listLength - 1 : prevIndex - 1
+          );
+        } else if (e.key === "Enter" && selectedIndex !== -1) {
+          onEnter(selectedIndex);
+        }
       }
-    }
-  };
+    },
+    [isDropdownVisible, listLength, selectedIndex, onEnter]
+  );
 
   useEffect(() => {
     window.addEventListener("keydown", handleKeyDown);
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [listLength, selectedIndex, isDropdownVisible]);
+  }, [handleKeyDown]);
 
   return selectedIndex;
 };

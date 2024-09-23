880a00cb073bb648282dcb31b9f1ad42fe40ac59
Gaurish Saini <gaurishprakhar@gmail.com>
2024-09-23 08:41:53 +0530

    fix: home navigation

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -1,12 +1,13 @@
 import React from "react";
 import brandLogo from "../assets/images/brandLogo.png";
+import { Link } from "react-router-dom";
 
 const Brand = () => {
   return (
-    <div className="flex items-center gap-2">
+    <Link to="/" className="flex items-center gap-2">
       <img src={brandLogo} alt="Brand Logo" className="w-6 h-6" />
       <div className="text-sm text-black dark:text-white">ByeWind</div>
-    </div>
+    </Link>
   );
 };
 

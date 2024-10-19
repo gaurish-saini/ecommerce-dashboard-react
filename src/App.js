3e3d4813426c2a20fb22cbc758ee31e3e102aa70
Gaurish Saini <gaurishprakhar@gmail.com>
2024-10-19 16:30:29 +0530

    fix: deploy preview

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { ThemeProvider } from "./context/ThemeContext";
 import {
   BrowserRouter as Router,
